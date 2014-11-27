2c7f0a893aa33a2f7cad6d3a738d23bcd02fccbb
Simen Skoglund <simen.skoglund@gmail.com>
2014-11-27 15:26:24 +0100

    done

--- sitbot.js
+++ sitbot.js
@@ -12,7 +12,6 @@ var server = http.createServer(function(req, res) {
   var d = new Date();
   var today = d.getDay();
   var parseString = require('xml2js').parseString;
-  var bro;
   requestify.get('https://www.sit.no/rss.ap?thisId=36444&lang=0&ma=on&ti=on&on=on&to=on&fr=on').then(function(response) {
     var xml = response.body;
     parseString(xml, function (err, result) {
@@ -22,27 +21,21 @@ var server = http.createServer(function(req, res) {
           middager[i] = middager[i].trim().split("</b>")[1];
      }
      hangaren = "*Hangaren*"+middager[today]+"\n";
-    // console.log(hangaren)
   });
 
   requestify.get('https://www.sit.no/rss.ap?thisId=36447&lang=0&ma=on&ti=on&on=on&to=on&fr=on').then(function(response) {
     var xml = response.body;
     parseString(xml, function (err, result) {
      realfag =result['rdf:RDF'].item[0].description[0].replace(/(\n *)+/g,'').replace(/\<br\>/g,'\n').replace(/:/g, ": ");
-     bro=realfag;
-     console.log(realfag);
      var middager = realfag.split("<b>");
      for(var i = 0; i < middager.length; i++) {
           middager[i] = middager[i].trim().split("</b>")[1];
      }
      realfag = "\n*Realfag*"+middager[today];
-     //console.log(realfag);
-    });
+    });   
 
-
-   var done = hangaren+realfag
   if(uri === '/') {
-    res.end(JSON.stringify({ "text": done }));
+    res.end(JSON.stringify({ "text": hangaren+realfag }));
     console.log('Request made for dinner');
   }
   
